cd1f72730db23c98f4e879c0d8b9f9098d6c1079
Eyoab Asrat <eyoabasrat@gmail.com>
2025-03-17 14:18:46 -0400

    Initial Commit

---
+++ spotchurchapp/spotchurchappApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotchurchappApp.swift
+//  spotchurchapp
+//
+//  Created by Eyoab Asrat on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct spotchurchappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
