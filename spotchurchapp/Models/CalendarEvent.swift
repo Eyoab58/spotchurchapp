8422563e5434d684d45b38ff44978a8ee7a07854
Eyoab Asrat <eyoabasrat@gmail.com>
2025-05-22 10:01:59 -0400

    "Added Info.plist file, removal of pod files from project, changed tls version so videos would be displayed"

---
+++ spotchurchapp/Models/CalendarEvent.swift
@@ -0,0 +1,8 @@
+//
+//  CalendarEvent.swift
+//  spotchurchapp
+//
+//  Created by Eyoab Asrat on 5/17/25.
+//
+
+import Foundation
---
+++ spotchurchapp/Models/YoutubeModels.swift
@@ -0,0 +1,18 @@
+//
+//  YoutubeModels.swift
+//  spotchurchapp
+//
+//  Created by Eyoab Asrat on 5/18/25.
+//
+
+import SwiftUI
+
+struct YoutubeModels: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YoutubeModels()
+}
