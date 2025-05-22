77590fa5ef71de3db07976a3230529d0b42ab843
Eyoab Asrat <eyoabasrat@gmail.com>
2025-05-22 12:03:26 -0400

    "Added manual events for calendar integration and also fixed frontend of calendar UI"

---
+++ spotchurchapp/Views/RemoteImageView.swift
@@ -0,0 +1,18 @@
+//
+//  RemoteImageView.swift
+//  spotchurchapp
+//
+//  Created by Eyoab Asrat on 5/22/25.
+//
+
+import SwiftUI
+
+struct RemoteImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RemoteImageView()
+}
