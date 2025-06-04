236a7844a2c8b37c0a2b3e69d5290a4990ec30cd
Eyoab Asrat <eyoabasrat@gmail.com>
2025-06-04 09:45:36 -0400

    "Begun the creation of the Engage tab with now the Baptism tab being fully developed"

---
+++ spotchurchapp/Views/BaptismView.swift
@@ -0,0 +1,18 @@
+//
+//  BaptismView.swift
+//  spotchurchapp
+//
+//  Created by Eyoab Asrat on 6/2/25.
+//
+
+import SwiftUI
+
+struct BaptismView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BaptismView()
+}
---
+++ spotchurchapp/Views/EngageView.swift
@@ -0,0 +1,18 @@
+//
+//  EngageView.swift
+//  spotchurchapp
+//
+//  Created by Eyoab Asrat on 6/2/25.
+//
+
+import SwiftUI
+
+struct EngageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EngageView()
+}
